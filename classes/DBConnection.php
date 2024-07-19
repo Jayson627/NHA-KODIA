3c0c7ea929b9bf69d6388b36cb3ce6055203e27c
Jayson627 <alcantarajayson118@gmail.com>
2024-07-19 15:00:37 +0900

    change

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -5,10 +5,10 @@ if (!defined('DB_SERVER')) {
 
 class DBConnection {
 
-    private $host = "127.0.0.1:3306";
-    private $username = "u510162695_sis_db";
-    private $password = "1Sis_dbpassword";
-    private $database = "u510162695_sis_db";
+    private $host = 127.0.0.1:3306;
+    private $username = u510162695_sis_db;
+    private $password = 1Sis_dbpassword;
+    private $database = u510162695_sis_db;
     
     public $conn;
     
@@ -20,8 +20,7 @@ class DBConnection {
             if ($this->conn->connect_error) {
                 die('Connection failed: ' . $this->conn->connect_error);
             }            
-        }   
-        else "connected",
+        }    
     }
     
     public function __destruct() {

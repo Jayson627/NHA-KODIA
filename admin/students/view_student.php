0c86e4c33a6620f0b07b9b57699ae1634580b8bb
Jayson627 <alcantarajayson118@gmail.com>
2024-07-19 22:06:14 +0900

    change

--- admin/students/view_student.php
+++ admin/students/view_student.php
@@ -185,9 +185,7 @@ if ($result->num_rows > 0) {
 }
 
 // Count total number of records
-$total_records_query = $conn -> query("SELECT COUNT(*) AS total FROM children");
-$total_records = $conn->query("SELECT COUNT(*) AS total FROM children")->fetch_assoc()['total'];
-echo "Total records: " . $total records";
+$total_records_query = $conn -> query("SELECT COUNT(*) AS total FROM childre")
 
 
 $conn->close();

d6687058a69a51973ac89ecf023e5c3c788d1b30
Jayson627 <alcantarajayson118@gmail.com>
2024-07-19 15:43:35 +0900

    change

--- inc/sess_auth.php
+++ inc/sess_auth.php
@@ -3,12 +3,12 @@
 //     session_start();
 // }
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') 
-    $link = "https://nha-kodia.com/"; 
+    $link = "https"; 
 else
     $link = "http"; 
 $link .= "://"; 
-$link .= $_SERVER['127.0.0.1:3306']; 
-$link .= $_SERVER['https://nha-kodia.com/'];
+$link .= $_SERVER['HTTP_HOST']; 
+$link .= $_SERVER['REQUEST_URI'];
 if(!strpos($link, 'login.php') && !strpos($link, 'register.php') && (!isset($_SESSION['userdata']) || (isset($_SESSION['userdata']['login_type']) && $_SESSION['userdata']['login_type'] != 2)) ){
 	redirect('login.php');
 }

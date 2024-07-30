bd378587633179c6170e9a2e9eef4e4bf59230ae
Jayson627 <alcantarajayson118@gmail.com>
2024-07-31 00:11:31 +0900

    change

--- admin/students/view_student.php
+++ admin/students/view_student.php
@@ -115,6 +115,14 @@ $username = "u510162695_sis_db";
 $password = "1Sis_dbpassword";
 $dbname = "u510162695_sis_db";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connections
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 // Initialize error message variable
 $error_message = "";
 

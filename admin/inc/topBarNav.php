07809e5b3268950cad78c09abab832b1f853ef41
Jayson627 <alcantarajayson118@gmail.com>
2024-09-16 13:57:02 +0900

    update

--- admin/inc/topBarNav.php
+++ admin/inc/topBarNav.php
@@ -28,19 +28,7 @@
 </style>
 
 <?php
-// Database credentials
-$servername = "127.0.0.1:3306";
-$username = "u510162695_sis_db";
-$password = "1Sis_dbpassword";
-$dbname = "u510162695_sis_db";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connections
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 // Get notification count for unread notifications
 $result = $conn->query("SELECT COUNT(*) as count FROM notifications WHERE is_read = 0");

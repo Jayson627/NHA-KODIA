52b810faf3aa4414e5ab72a71ffe6eff427d15f9
Jayson627 <alcantarajayson118@gmail.com>
2024-09-21 07:58:18 +0900

    update

--- admin/process_resident.php
+++ admin/process_resident.php
@@ -1,7 +1,20 @@
-<?php 
-// Database connection
-include_once('connection.php'); 
+<?php
+// Database credentials
+$servername = "127.0.0.1:3306";
+$username = "u510162695_sis_db";
+$password = "1Sis_dbpassword";
+$dbname = "u510162695_sis_db";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Initialize error message variable
+$error_message = "";
 
 
 // Fetch announcements from the database

fe1683e932698497eb70c7b7371256ee954eb59f
Jayson627 <alcantarajayson118@gmail.com>
2024-10-12 10:10:36 +0900

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
                       
                     } else if (resp.status == 'incorrect') {
                         var _frm = $('#login-frm')
-                        var _msg = "<div class='alert alert-danger text-white err_msg'><i class='fa fa-exclamation-triangle'></i> Incorrect username or password</div>"
+                        var _msg = "<div class='alert alert-danger text-white err_msg'><i class='fa fa-exclamation-triangle'></i> kakakakkakaka</div>"
                         _frm.prepend(_msg)
                         _frm.find('input').addClass('is-invalid')
                         $('[name="username"]').focus()

0beb3ae3c4677ac2aba85037ab174afacd1a763a
Jayson627 <alcantarajayson118@gmail.com>
2024-10-21 14:45:43 +0900

    update

--- admin/block.php
+++ admin/block.php
@@ -1,20 +1,9 @@
 <?php
 include_once('connection.php'); 
 
-// Database credentials
-$servername = "127.0.0.1:3306";
-$username = "u510162695_sis_db";
-$password = "1Sis_dbpassword";
-$dbname = "u510162695_sis_db";
+// Initialize error message variable
+$error_message = "";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connections
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    
-}
 // Pagination setup
 $limit = 10; 
 $page = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1; 

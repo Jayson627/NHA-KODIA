e033c3b87d666d699219863f52a1035c0bc38ab0
Jayson627 <alcantarajayson118@gmail.com>
2024-10-26 10:03:14 +0900

    update

--- admin/people_dashboard.php
+++ admin/people_dashboard.php
@@ -3,13 +3,6 @@
 <?php
 include_once('connection.php'); 
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 // Fetch announcements from the database
 $sql = "SELECT id, title, content, date FROM announcements ORDER BY date DESC";

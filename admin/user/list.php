31aad365576e129a68b2f0be4493e0d778d0884a
Jayson627 <alcantarajayson118@gmail.com>
2024-11-11 08:29:43 +0900

    update

--- admin/user/list.php
+++ admin/user/list.php
@@ -57,7 +57,7 @@
 						<th>Avatar</th>
 						<th>Name</th>
 						<th>Email</th>
-						<th>User Type</th>
+						<th>User</th>
 						<th>Action</th>
 					</tr>
 				</thead>
@@ -74,7 +74,7 @@
 							</td>
 							<td data-label="Name"><?php echo ucwords($row['name']) ?></td>
 							<td data-label="email"><p class="m-0 truncate-1"><?php echo $row['email'] ?></p></td>
-							<td data-label="User Type"><p class="m-0"><?php echo ($row['type'] == 1 )? "Administrator" : "Staff" ?></p></td>
+							<td data-label="User Type"><p class="m-0"><?php echo ($row['type'] == 1 )? "Administrator" : "" ?></p></td>
 							<td align="center" data-label="Action">
 								<button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
 									Action

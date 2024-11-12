f9feea1f479bff5d1e988a3c81396346eec0f166
Jayson627 <alcantarajayson118@gmail.com>
2024-11-13 08:54:22 +0900

    update

--- admin/user/list.php
+++ admin/user/list.php
@@ -2,7 +2,7 @@
 <script>
 	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
 </script>
-<?php endif; ?>
+<?php endif;?>
 
 <style>
     .img-avatar {
@@ -10,132 +10,92 @@
         height: 45px;
         object-fit: cover;
         object-position: center center;
-        border-radius: 50%;
+        border-radius: 0;
     }
 
     @media (max-width: 768px) {
         .table thead {
             display: none; /* Hide the header on small screens */
         }
-
         .table, .table tbody, .table tr, .table td {
             display: block; /* Make each row a block */
             width: 100%; /* Full width */
         }
-
         .table tr {
             margin-bottom: 15px; /* Space between rows */
             border: 1px solid #dee2e6; /* Add a border */
-            border-radius: 5px;
-            padding: 10px 0;
         }
-
         .table td {
             text-align: right; /* Right align text */
             position: relative; /* Position relative for pseudo-elements */
             padding-left: 50%; /* Add padding for the label */
-            padding-right: 20px; /* Add space on the right */
-            border: none; /* Remove borders between cells */
         }
-
         .table td::before {
             content: attr(data-label); /* Use data-label for each cell */
             position: absolute; /* Position absolute for the label */
-            left: 10px;
-            top: 0;
+            left: 0;
+            padding-left: 10px;
             font-weight: bold;
             text-align: left; /* Align to the left */
-        }
-
-        .dropdown-menu {
-            min-width: 120px;
-        }
-
-        .dropdown-item {
-            padding: 8px 12px;
-            font-size: 14px;
-        }
-
-        .table td p {
-            margin: 0;
-            font-size: 14px;
-        }
-
-        .table .btn-sm {
-            font-size: 12px;
-            padding: 5px 10px;
-        }
-
-        .img-avatar {
-            width: 30px;
-            height: 30px;
-        }
-    }
-
-    /* Mobile Optimized Card Header */
-    @media (max-width: 768px) {
-        .card-header {
-            padding: 10px;
-            font-size: 16px;
         }
     }
 </style>
 
 <div class="card card-outline card-primary">
-    <div class="card-header">
-        <h3 class="card-title">List of System Users</h3>
-        <div class="card-tools">
-            <a href="?page=user/manage_user" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New</a>
-        </div>
-    </div>
-    <div class="card-body">
-        <div class="container-fluid">
-            <table class="table table-hover table-striped">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Avatar</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>User</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php 
-                        $i = 1;
-                        $qry = $conn->query("SELECT *,concat(firstname,' ',lastname) as name from `users` where id != '1' order by concat(firstname,' ',lastname) asc ");
-                        while($row = $qry->fetch_assoc()):
-                    ?>
-                        <tr>
-                            <td class="text-center" data-label="#"><?php echo $i++; ?></td>
-                            <td class="text-center" data-label="Avatar">
-                                <img src="<?php echo validate_image($row['avatar']) ?>" class="img-avatar img-thumbnail p-0 border-2" alt="user_avatar">
-                            </td>
-                            <td data-label="Name"><?php echo ucwords($row['name']) ?></td>
-                            <td data-label="Email"><p class="m-0 truncate-1"><?php echo $row['email'] ?></p></td>
-                            <td data-label="User Type"><p class="m-0"><?php echo ($row['type'] == 1 )? "Administrator" : "" ?></p></td>
-                            <td align="center" data-label="Action">
-                                <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
-                                    Action
-                                    <span class="sr-only">Toggle Dropdown</span>
-                                </button>
-                                <div class="dropdown-menu" role="menu">
-                                    <a class="dropdown-item" href="?page=user/manage_user&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
-                                    <div class="dropdown-divider"></div>
-                                    <?php if($row['status'] != 1): ?>
-                                        <a class="dropdown-item verify_user" href="javascript:void(0)" data-id="<?= $row['id'] ?>" data-name="<?= $row['email'] ?>"><span class="fa fa-check text-primary"></span> Verify</a>
-                                        <div class="dropdown-divider"></div>
-                                    <?php endif; ?>
-                                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
-                                </div>
-                            </td>
-                        </tr>
-                    <?php endwhile; ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
+	<div class="card-header">
+		<h3 class="card-title">List of System Users</h3>
+		<div class="card-tools">
+			<a href="?page=user/manage_user" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New</a>
+		</div>
+	</div>
+	<div class="card-body">
+		<div class="container-fluid">
+			<table class="table table-hover table-striped">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Avatar</th>
+						<th>Name</th>
+						<th>Email</th>
+						<th>User</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+						$i = 1;
+						$qry = $conn->query("SELECT *,concat(firstname,' ',lastname) as name from `users` where id != '1' order by concat(firstname,' ',lastname) asc ");
+						while($row = $qry->fetch_assoc()):
+					?>
+						<tr>
+							<td class="text-center" data-label="#"><?php echo $i++; ?></td>
+							<td class="text-center" data-label="Avatar">
+								<img src="<?php echo validate_image($row['avatar']) ?>" class="img-avatar img-thumbnail p-0 border-2" alt="user_avatar">
+							</td>
+							<td data-label="Name"><?php echo ucwords($row['name']) ?></td>
+							<td data-label="email"><p class="m-0 truncate-1"><?php echo $row['email'] ?></p></td>
+							<td data-label="User Type"><p class="m-0"><?php echo ($row['type'] == 1 )? "Administrator" : "" ?></p></td>
+							<td align="center" data-label="Action">
+								<button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
+									Action
+									<span class="sr-only">Toggle Dropdown</span>
+								</button>
+								<div class="dropdown-menu" role="menu">
+									<a class="dropdown-item" href="?page=user/manage_user&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
+									<div class="dropdown-divider"></div>
+									<?php if($row['status'] != 1): ?>
+										<a class="dropdown-item verify_user" href="javascript:void(0)" data-id="<?= $row['id'] ?>" data-name="<?= $row['email'] ?>"><span class="fa fa-check text-primary"></span> Verify</a>
+										<div class="dropdown-divider"></div>
+									<?php endif; ?>
+									<a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
+								</div>
+							</td>
+						</tr>
+					<?php endwhile; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
 
 <script>

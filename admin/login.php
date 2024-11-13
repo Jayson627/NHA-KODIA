472447f99c72ffa35f586deb5320206cc9d857c7
Jayson627 <alcantarajayson118@gmail.com>
2024-11-13 15:43:46 +0900

    update

--- admin/login.php
+++ admin/login.php
@@ -392,10 +392,9 @@
             </div>
           </div>
           <div class="form-group">
-          
-            <button type="submit" class="btn btn-primary btn-block">Signhgfh In</button>
-          </div>
           <div class="g-recaptcha" data-sitekey="6LceIn0qAAAAAE_rSc2kZXmXjUvujL48bo7mKYE5"></div>
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
           <div class="form-group text-center">
             <a href="forgot_password.php" class="text-primary">Forgot Password?</a>
           </div>

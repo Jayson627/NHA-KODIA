3bbee151c6d8328542dc157200f5728cac3bcdb4
Jayson627 <alcantarajayson118@gmail.com>
2024-11-14 20:22:58 +0900

    update

--- admin/user/list.php
+++ admin/user/list.php
@@ -71,7 +71,7 @@
 						<td class="text-center"><?php echo $i++; ?></td>
 							<td class="text-center"><img src="<?php echo validate_image($row['avatar']) ?>" class="img-avatar img-thumbnail p-0 border-2" alt="user_avatar"></td>
 							<td><?php echo ucwords($row['name']) ?></td>
-							<td ><p class="m-0 truncate-1"><?php echo $row['username'] ?></p></td>
+							<td ><p class="m-0 truncate-1"><?php echo $row['email'] ?></p></td>
 							<td ><p class="m-0"><?php echo ($row['type'] == 1 )? "Adminstrator" : "Staff" ?></p></td>
 							<td align="center">
 								 <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">

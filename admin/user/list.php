98d2d85e7bd9839b5b4a4a6fec8e5d1721bb7feb
Jayson627 <alcantarajayson118@gmail.com>
2024-11-17 08:27:53 +0900

    fdsfds

--- admin/user/list.php
+++ admin/user/list.php
@@ -1,8 +1,8 @@
 <?php if($_settings->chk_flashdata('success')): ?>
 <script>
-	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
+    alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
 </script>
-<?php endif;?>
+<?php endif; ?>
 
 <style>
     .img-avatar {
@@ -42,119 +42,132 @@
 </style>
 
 <div class="card card-outline card-primary">
-	<div class="card-header">
-		<h3 class="card-title">List of System Users</h3>
-		<div class="card-tools">
-			<a href="?page=user/manage_user" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New</a>
-		</div>
-	</div>
-	<div class="card-body">
-		<div class="container-fluid">
-			<table class="table table-hover table-striped">
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>Avatar</th>
-						<th>Name</th>
-						<th>Email</th>
-						<th>User </th>
-						<th>Action</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php 
-						$i = 1;
-						$qry = $conn->query("SELECT *,concat(firstname,' ',lastname) as name from `users` where id != '1' order by concat(firstname,' ',lastname) asc ");
-						while($row = $qry->fetch_assoc()):
-					?>
-						<tr>
-						<td class="text-center"><?php echo $i++; ?></td>
-							<td class="text-center"><img src="<?php echo validate_image($row['avatar']) ?>" class="img-avatar img-thumbnail p-0 border-2" alt="user_avatar"></td>
-							<td><?php echo ucwords($row['name']) ?></td>
-							<td ><p class="m-0 truncate-1"><?php echo $row['email'] ?></p></td>
-							<td ><p class="m-0"><?php echo ($row['type'] == 1 )? "Adminstrator" : "Staff" ?></p></td>
-							<td align="center">
-								<button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
-									Action
-									<span class="sr-only">Toggle Dropdown</span>
-								</button>
-								<div class="dropdown-menu" role="menu">
-									<a class="dropdown-item" href="?page=user/manage_user&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
-									<div class="dropdown-divider"></div>
-									<?php if($row['status'] != 1): ?>
-										<a class="dropdown-item verify_user" href="javascript:void(0)" data-id="<?= $row['id'] ?>" data-name="<?= $row['email'] ?>"><span class="fa fa-check text-primary"></span> Verify</a>
-										<div class="dropdown-divider"></div>
-									<?php endif; ?>
-									<a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
-								</div>
-							</td>
-						</tr>
-					<?php endwhile; ?>
-				</tbody>
-			</table>
-		</div>
-	</div>
+    <div class="card-header">
+        <h3 class="card-title">List of System Users</h3>
+        <div class="card-tools">
+            <a href="?page=user/manage_user" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New</a>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="container-fluid">
+            <table class="table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Avatar</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>User </th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        $i = 1;
+                        $qry = $conn->query("SELECT *,concat(firstname,' ',lastname) as name from `users` where id != '1' order by concat(firstname,' ',lastname) asc ");
+                        while($row = $qry->fetch_assoc()):
+                    ?>
+                        <tr>
+                            <td class="text-center"><?php echo $i++; ?></td>
+                            <td class="text-center"><img src="<?php echo validate_image($row['avatar']) ?>" class="img-avatar img-thumbnail p-0 border-2" alt="user_avatar"></td>
+                            <td><?php echo ucwords($row['name']) ?></td>
+                            <td ><p class="m-0 truncate-1"><?php echo $row['email'] ?></p></td>
+                            <td ><p class="m-0"><?php echo ($row['type'] == 1 )? "Adminstrator" : "Staff" ?></p></td>
+                            <td align="center">
+                                <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
+                                    Action
+                                    <span class="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div class="dropdown-menu" role="menu">
+                                    <a class="dropdown-item" href="?page=user/manage_user&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
+                                    <div class="dropdown-divider"></div>
+                                    <?php if($row['status'] != 1): ?>
+                                        <a class="dropdown-item verify_user" href="javascript:void(0)" data-id="<?= $row['id'] ?>" data-name="<?= $row['email'] ?>"><span class="fa fa-check text-primary"></span> Verify</a>
+                                        <div class="dropdown-divider"></div>
+                                    <?php endif; ?>
+                                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
+
 <script>
-	$(document).ready(function(){
-		$('.delete_data').click(function(){
-			_conf("Are you sure to delete this User permanently?","delete_user",[$(this).attr('data-id')])
-		});
-		$('.table td,.table th').addClass('py-1 px-2 align-middle');
-		$('.table').dataTable({
-			responsive: true, // Ensure DataTable is responsive
-			searching: false, // Disable search box
-			lengthChange: false, // Disable "Show entries" dropdown
-			pageLength: 10, // Set default number of rows to display
-		});
-		$('.verify_user').click(function(){
-			_conf("Are you sure to verify <b>"+$(this).attr('data-name')+"<b/>?","verify_user",[$(this).attr('data-id')])
-		});
-	});
+    $(document).ready(function() {
+        // Initialize the DataTable
+        var table = $('.table').DataTable({
+            responsive: true,       // Ensure table is responsive
+            searching: false,       // Disable search box
+            lengthChange: false,    // Disable "Show entries" dropdown
+            pageLength: 10,         // Set the default number of rows per page
+            paging: true,           // Ensure pagination is enabled
+            info: true,             // Show table info (e.g., showing x to y of z entries)
+            language: {
+                paginate: {
+                    previous: "<",   // Customize the previous button text
+                    next: ">"        // Customize the next button text
+                }
+            }
+        });
 
-	function delete_user($id){
-		start_loader();
-		$.ajax({
-			url:_base_url_+"classes/Users.php?f=delete",
-			method:"POST",
-			data:{id: $id},
-			dataType:"json",
-			error: err => {
-				console.log(err);
-				alert_toast("An error occurred.",'error');
-				end_loader();
-			},
-			success:function(resp){
-				if(typeof resp == 'object' && resp.status == 'success'){
-					location.reload();
-				}else{
-					alert_toast("An error occurred.",'error');
-					end_loader();
-				}
-			}
-		});
-	}
+        // Delete action
+        $('.delete_data').click(function(){
+            _conf("Are you sure to delete this User permanently?","delete_user",[$(this).attr('data-id')])
+        });
 
-	function verify_user($id){
-		start_loader();
-		$.ajax({
-			url:_base_url_+"classes/Users.php?f=verify_user",
-			method:"POST",
-			data:{id: $id},
-			dataType:"json",
-			error: err => {
-				console.log(err);
-				alert_toast("An error occurred.",'error');
-				end_loader();
-			},
-			success:function(resp){
-				if(typeof resp == 'object' && resp.status == 'success'){
-					location.reload();
-				}else{
-					alert_toast("An error occurred.",'error');
-					end_loader();
-				}
-			}
-		});
-	}
+        // Verify action
+        $('.verify_user').click(function(){
+            _conf("Are you sure to verify <b>"+$(this).attr('data-name')+"<b/>?","verify_user",[$(this).attr('data-id')])
+        });
+    });
+
+    function delete_user($id){
+        start_loader();
+        $.ajax({
+            url:_base_url_+"classes/Users.php?f=delete",
+            method:"POST",
+            data:{id: $id},
+            dataType:"json",
+            error: err => {
+                console.log(err);
+                alert_toast("An error occurred.",'error');
+                end_loader();
+            },
+            success:function(resp){
+                if(typeof resp == 'object' && resp.status == 'success'){
+                    location.reload();
+                }else{
+                    alert_toast("An error occurred.",'error');
+                    end_loader();
+                }
+            }
+        });
+    }
+
+    function verify_user($id){
+        start_loader();
+        $.ajax({
+            url:_base_url_+"classes/Users.php?f=verify_user",
+            method:"POST",
+            data:{id: $id},
+            dataType:"json",
+            error: err => {
+                console.log(err);
+                alert_toast("An error occurred.",'error');
+                end_loader();
+            },
+            success:function(resp){
+                if(typeof resp == 'object' && resp.status == 'success'){
+                    location.reload();
+                }else{
+                    alert_toast("An error occurred.",'error');
+                    end_loader();
+                }
+            }
+        });
+    }
 </script>

eed71d2d85e80070eef4f57e547c66c88026e954
Jayson627 <alcantarajayson118@gmail.com>
2024-11-20 23:59:22 +0900

    c

--- admin/funtion.php
+++ admin/funtion.php
@@ -1,90 +1,107 @@
 <?php
-session_start();
-include "mailer.php";
+require_once("mailer.php");
 require_once('../admin/connection.php');
 require_once("../initialize.php");
-if (isset($_POST["btn-forgotpass"])) {
-    $email = $_POST["email"];
-    
-    $allowed_gmail = "alcantarajayson118@gmail.com";
-    if ($email !== $allowed_gmail) {
-        // If the email doesn't match, show a message or redirect
-        $_SESSION["notify"] = "Email not found! Please contact the administrator to reset a password.";
-        header("Location: ../admin/forgot_password.php");
-        exit();
-    }
 
+if (isset($_POST["btn-forgotpass"])) {
+
+    $email = $_POST["email"];
     $reset_code = random_int(100000, 999999);
     
     $sql = "UPDATE `users` SET `code`='$reset_code' WHERE email='$email'";
  
     $query = mysqli_query($conn, $sql);
  
+ 
     if ($query) {
         
-        //Set Params 
-        $mail->SetFrom("sscvoting@do-not.reply");
+        //Set Paramss oki na pre
+        $mail->SetFrom("alcantarajayson118@gmail.com");
         $mail->AddAddress("$email");
         $mail->Subject = "Reset Password OTP";
         $mail->Body = "Use this OTP Code to reset your password: ".$reset_code."<br/>".
-        "Click the link to reset password: http://nha-kodia.com/admin/reset_password?reset&email=$email"  //pulihan $reset_coede
+        "Click the link to reset password: http://nha-kodia.com/admin/reset_password.php?reset&email=$reset_code" 
         ;
 
 
-        if (!$mail->send()) {
-            $_SESSION["notify"] = "Mailer Error: " . $mail->ErrorInfo;
+        if(!$mail->Send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
         } else {
-            $_SESSION["notify"] = "A reset link has been sent to your email.";
+            echo "Message has been sent";
         }
 
-        // Redirect to the forgot password page
-        header("Location: ../admin/forgot_password.php");
-        exit();
-    } else {
-        $_SESSION["notify"] = "Failed to update the reset code. Please try again.";
-        header("Location: ../admin/forgot_password.php");
-        exit();
+        //OTP has been sent please check your email
+        $_SESSION["notify"] = "success";
+ 
+        header("location: ../admin/forgot_password.php");
+ 
+    }else {
+ 
+        $_SESSION["notify"] = "failed";
+        
+ 
+        header("location: ../admin/forgot_password.php");
+ 
+ 
     }
-}
-if (isset($_POST["btn-new-password"])) {
+ 
+ }
+ // new password 
+ if (isset($_POST["btn-new-password"])) {
+
     $email = $_POST["email"];
     $password = $_POST["password"];
     $otp = $_POST["otp"];
 
-    // Prepare the SQL statement to avoid SQL injection
-    $stmt = $conn->prepare("SELECT `code` FROM `users` WHERE email = ?");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
 
-    if ($result->num_rows > 0) {
-        $res = $result->fetch_assoc();
-        $get_code = $res["code"];
+
+    $sql = "SELECT `code` FROM `users` WHERE email='$email'";
+
+    $query = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($query) > 0) {
+
+        while ($res = mysqli_fetch_assoc($query)) {
+
+            $get_code = $res["code"];
+
+        }
 
         if ($otp === $get_code) {
-            $reset = random_int(100000, 999999);
-            $password = password_hash($password, PASSWORD_DEFAULT);
 
-            // Prepare the update statement to avoid SQL injection
-            $update_stmt = $conn->prepare("UPDATE `users` SET `password` = ?, `code` = ? WHERE email = ?");
-            $update_stmt->bind_param("sis", $password, $reset, $email);
-            
-            if ($update_stmt->execute()) {
-                $_SESSION["notify"] = "Your password has been reset successfully.";
-            } else {
-                $_SESSION["notify"] = "Failed to update password. Please try again.";
-            }
-            header("Location: ../admin/fotgot_password.php");
-            exit();
-        } else {
-            $_SESSION["notify"] = "Invalid OTP. Please try again.";
-            header("Location: ../admin/forgot_password.php");
-            exit();
+           
+
+            $reset = random_int(100000, 999999);
+             $password = password_hash($password, PASSWORD_DEFAULT);
+
+            $sql = "UPDATE `users` SET `password`='$password', `code`=$reset  WHERE email='$email'";
+
+            $query = mysqli_query($conn, $sql);
+
+            $_SESSION["notify"] = "success";
+
+            header("location: ../admin/forgot_password.php");
+ 
+ 
+        }else {
+
+            $_SESSION["notify"] = "invalid";
+
+            header("location: ../admin/forgot_password.php");
+ 
+
         }
-    } else {
-        $_SESSION["notify"] = "Email not found.";
-        header("Location: ../admin/forgot_password.php");
-        exit();
+
+    }else {
+
+            $_SESSION["notify"] = "invalid";
+
+            header("location: ../admin/forgot_password.php");
+ 
+ 
+
     }
 }
+
+
 ?>

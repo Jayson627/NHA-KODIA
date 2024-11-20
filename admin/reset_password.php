c788037ac46d60fa8a272a61fea140b34ba16d19
Jayson627 <alcantarajayson118@gmail.com>
2024-11-20 23:57:31 +0900

    c

--- admin/reset_password.php
+++ admin/reset_password.php
@@ -71,7 +71,7 @@ if (isset($_GET["reset"])) {
 <body>
     <div class="reset-password-box">
         <h2 class="reset-password-title">Reset Password</h2>
-        <form action="../admin/function.php" method="POST">
+        <form action="../admin/funtion.php" method="POST">
             <div class="form-group has-feedback">
                 <input type="hidden" name="email" class="form-control" value="<?php echo htmlspecialchars($email); ?>" required readonly>
             </div>

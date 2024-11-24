4105c4aa797247ed02dc33e3a458ea119cf67a2f
Jayson627 <alcantarajayson118@gmail.com>
2024-11-24 21:11:50 +0900

    c

--- admin/filter_block.php
+++ admin/filter_block.php
@@ -23,6 +23,10 @@ if ($conn->connect_error) {
     <title>Filter by Block</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
+<a href="president" class="btn">
+    <i class="fas fa-arrow-left"></i> Back
+</a>
+
 <body class="bg-light">
 
 <div class="container mt-5">

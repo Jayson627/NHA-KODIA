2cd750aac22891cb29fbd2454b2c1f4506ce9fdd
Jayson627 <alcantarajayson118@gmail.com>
2024-11-25 21:19:40 +0900

    hghgh

--- admin/forgot_password.php
+++ admin/forgot_password.php
@@ -106,6 +106,20 @@
       });
     });
   </script> -->
+  <script>
+
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'C' || e.key === 'J')) || (e.ctrlKey && e.key === 'U')) {
+        e.preventDefault();
+    }
+});
+
+
+    </script>
 
 </body>
 </html>

6307c44b70ead6056b8e3a3690a02374e08360a5
Jayson627 <alcantarajayson118@gmail.com>
2024-11-25 11:34:50 +0900

    cc

--- admin/block.php
+++ admin/block.php
@@ -88,7 +88,7 @@ $total_records = $conn->query("SELECT COUNT(*) AS total FROM blocks")->fetch_ass
 
 $conn->close();
 ?>
-<a href="index" class="btn btn-secondary">Back</a>
+<a href="index.php" class="btn btn-secondary">Back</a>
 <!DOCTYPE html>
 <html lang="en">
 <head>

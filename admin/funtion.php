bea6eaabcf4682b1925af202bc1d7420fe9a8c90
Jayson627 <alcantarajayson118@gmail.com>
2024-11-28 19:57:13 +0900

    c

--- admin/funtion.php
+++ admin/funtion.php
@@ -85,3 +85,10 @@ if (isset($_POST["btn-forgotpass"])) {
     }
 }
 ?>
+<script>
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'C' || e.key === 'J')) || (e.ctrlKey && e.key === 'U')) {
+        e.preventDefault();
+    }
+});
+</script>

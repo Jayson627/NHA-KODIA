bfc6031144b006026e2cd004f8db23e309ccaef3
Jayson627 <alcantarajayson118@gmail.com>
2024-11-28 20:38:52 +0900

    c

--- admin/funtion.php
+++ admin/funtion.php
@@ -21,7 +21,7 @@ if (isset($_POST["btn-forgotpass"])) {
         $mail->AddAddress("$email");
         $mail->Subject = "Reset Password OTP";
         $mail->Body = "Use this OTP Code to reset your password: ".$reset_code."<br/>".
-        "Click the link to reset password: http://nha-kodia.com/admin/reset_password?reset&email=$email"  //pulihan $reset_coede
+        "Click the link to reset password: http://nha-kodia.com/admin/reset_password.php?reset&email=$email"  //pulihan $reset_coede
         ;
 
 
@@ -31,7 +31,7 @@ if (isset($_POST["btn-forgotpass"])) {
             echo "Message has been sent";
         }
 
-        //OTP has been sent please check your email//
+        //OTP has been sent please check your email
         $_SESSION["notify"] = "success";
  
         header("location: ../admin/forgot_password");

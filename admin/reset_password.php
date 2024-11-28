67c8af025e55d838b32244a8fe389c44505857aa
Jayson627 <alcantarajayson118@gmail.com>
2024-11-28 20:02:51 +0900

    c

--- admin/reset_password.php
+++ admin/reset_password.php
@@ -221,7 +221,7 @@ document.addEventListener('keydown', function (e) {
     if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'C' || e.key === 'J')) || (e.ctrlKey && e.key === 'U')) {
         e.preventDefault();
     }
-});
+});f
 
   </script>
 </body>h

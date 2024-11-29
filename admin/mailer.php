fac70829456651ff387eb7b5733e4608fb8d8afb
Jayson627 <alcantarajayson118@gmail.com>
2024-11-29 21:11:32 +0900

    New Changes

--- admin/mailer.php
+++ admin/mailer.php
@@ -1,24 +1,24 @@
 <?php
 // DO NOT TOUCH THIS SECTION ~ 
-//These must be at the top of your script, not inside a function
+// These must be at the top of your script, not inside a function
 require("PHPMailer/src/PHPMailer.php");
 require("PHPMailer/src/SMTP.php");
 require("PHPMailer/src/Exception.php");
 
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-$mail->IsSMTP(); 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-$mail->CharSet="UTF-8";
-$mail->Host = "smtp.gmail.com";
-$mail->SMTPDebug = 1; 
-$mail->Port = 465 ; //465 or 587
+$mail = new PHPMailer(true);  
 
-$mail->SMTPSecure = 'ssl';  
+$mail->CharSet = "UTF-8";
+$mail->IsSMTP();  
+$mail->Host = "smtp.gmail.com"; 
+$mail->Port = 587;  
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
 $mail->SMTPAuth = true; 
+
+$mail->Username = "alcantarajayson118@gmail.com";  
+$mail->Password = "xbdldpzpvsdhicxd"; 
+
+
 $mail->IsHTML(true);
-
-$mail->Username = "alcantarajayson118@gmail.com";
-$mail->Password = "xbdldpzpvsdhicxd";
-
-
-?>

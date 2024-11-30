c699b1004726646812f1216a2bbc22065f292718
Jayson627 <alcantarajayson118@gmail.com>
2024-11-30 14:40:19 +0900

    CHANGE

--- admin/login.php
+++ admin/login.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <?php require_once('../config.php'); ?>
 <?php require_once('inc/header.php'); ?>
-<head>
+<Chead>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Login</title>

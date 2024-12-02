c20c878adb60f45a1ab5c5e11e123df7f3623b97
Jayson627 <alcantarajayson118@gmail.com>
2024-12-02 12:34:40 +0900

    c

--- classes/Users.php
+++ classes/Users.php
@@ -13,7 +13,7 @@ class Users extends DBConnection {
     public function __destruct(){
         parent::__destruct();
     }
-
+}
     public function save_users(){
         if(!isset($_POST['status']) && $this->settings->userdata('login_type') == 1){
             $_POST['status'] = 1;
@@ -141,8 +141,7 @@ class Users extends DBConnection {
         } else {
             $resp['status'] = 'failed';
         }
-        return json_encode($resp);
-    } 
+        return json_encode($resp); tulug anay ako pre sara kuna tiwasun cge pre oo pre
 
     public function save_employee(){
         if(!empty($_POST['password'])){

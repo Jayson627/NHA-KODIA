3218fe58e259eb15c2044fc66465679b5a9ffae2
Jayson627 <alcantarajayson118@gmail.com>
2024-12-10 22:13:09 +0900

    c

--- admin/system_info/index.php
+++ admin/system_info/index.php
@@ -47,13 +47,7 @@
 			<div class="form-group d-flex justify-content-center">
 				<img src="<?php echo validate_image($_settings->info('logo')) ?>" alt="" id="cimg" class="img-fluid img-thumbnail">
 			</div>
-			<div class="form-group">
-    <label for="" class="control-label">Cover</label>
-    <div class="custom-file">
-        <input type="file" class="custom-file-input rounded-circle" id="coverFile" name="cover" accept=".jpg, .jpeg, .png" onchange="displayImg2(this,$(this))">
-        <label class="custom-file-label" for="coverFile">Choose file</label>
-    </div>
-</div>
+			
 			<div class="form-group d-flex justify-content-center">
 				<img src="<?php echo validate_image($_settings->info('cover')) ?>" alt="" id="cimg2" class="img-fluid img-thumbnail bg-gradient-dark border-blue">
 			</div>

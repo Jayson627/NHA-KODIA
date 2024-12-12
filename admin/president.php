ef45bb12579f7fc38f3aea7af6f3f03b991ac508
Jayson627 <alcantarajayson118@gmail.com>
2024-12-12 12:30:53 +0900

    c

--- admin/president.php
+++ admin/president.php
@@ -1,23 +1,6 @@
-
 <?php
-// Database credentials
-$servername = "127.0.0.1:3306";
-$username = "u510162695_sis_db";
-$password = "1Sis_dbpassword";
-$dbname = "u510162695_sis_db";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connections
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Initialize error message variable
-$error_message = "";
 session_start();
-
+include_once('connection.php');
 
 // Fetch announcements from the database
 $announcementQuery = "SELECT * FROM announcement ORDER BY created_at DESC";

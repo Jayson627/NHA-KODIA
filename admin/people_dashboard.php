9ae71b7a25ea6cf236dcdc2e6939d7940fd68482
Jayson627 <alcantarajayson118@gmail.com>
2024-12-12 12:16:46 +0900

    c

--- admin/people_dashboard.php
+++ admin/people_dashboard.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once('connection.php');
+include_once('../admin/connection');
 
 // Fetch announcements from the database
 $announcementQuery = "SELECT * FROM announcement ORDER BY created_at DESC";

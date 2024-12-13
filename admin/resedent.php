952aa2a4773fac6238d09c36895a819c54e82197
Jayson627 <alcantarajayson118@gmail.com>
2024-12-13 12:35:13 +0900

    c

--- admin/resedent.php
+++ admin/resedent.php
@@ -125,10 +125,10 @@
                 <input type="password" id="login_password" name="login_password" required>
 
                 <input type="submit" value="Login">
-                <a href="forget_password" style="color: #5a67d8; text-decoration: underline;">Forgot Password?</a>
             </form>
-            <a href="forget_password" style="color: #5a67d8; text-decoration: underline;">Forgot Password?</a>
+           
             <div class="toggle-link" onclick="toggleForms()">Don't have an account? Register here</div>
+            <a href="forget_password" style="color: #5a67d8; text-decoration: underline;">Forgot Password?</a>
         </div>
     </div>
 

b97c1e5b3529d92b46c45e80574d98df4ad37302
Jayson627 <alcantarajayson118@gmail.com>
2024-12-17 20:45:43 +0900

    c

--- admin/home.php
+++ admin/home.php
@@ -154,7 +154,12 @@
                     'rgba(0, 123, 255, 0.6)',   // Total Males
                     'rgba(255, 99, 132, 0.6)'   // Total Females
                 ],
-               
+                borderColor: [
+                    
+                    'rgba(0, 123, 255, 1)',  // Male border color
+                    'rgba(255, 99, 132, 1)'   // Female border color
+                ],
+                borderWidth: 1
             }]
         };
 

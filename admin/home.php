4a9228e8a7097a4f6d3837521c76cba198afa6ad
Jayson627 <alcantarajayson118@gmail.com>
2024-12-17 21:11:16 +0900

    c

--- admin/home.php
+++ admin/home.php
@@ -189,8 +189,24 @@
                     <?php echo $total_governmentemployee; ?>,
                     <?php echo $total_unemployed; ?>
                 ],
-                backgroundColor: 'rgba(75, 192, 192, 0.6)',
-                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',   // Farmer
+                    'rgba(54, 162, 235, 0.6)',   // Fisherman
+                    'rgba(255, 206, 86, 0.6)',   // Carpenter
+                    'rgba(75, 192, 192, 0.6)',   // Vendor
+                    'rgba(153, 102, 255, 0.6)',  // Driver
+                    'rgba(255, 159, 64, 0.6)',   // Government Employee
+                    'rgba(201, 203, 207, 0.6)'   // Unemployed
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(201, 203, 207, 1)'
+                ],
                 borderWidth: 1
             }]
         };
